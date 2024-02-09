c1cf37a70327147ae0804ebd4be59d24c4239798
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-02-09 17:42:22 +0600

    firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,13 +5,21 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC-peKfaq8qEMeucCnMS9RAkShLuZ569Qw",
-  authDomain: "travel-guru-b8.firebaseapp.com",
-  projectId: "travel-guru-b8",
-  storageBucket: "travel-guru-b8.appspot.com",
-  messagingSenderId: "352518348087",
-  appId: "1:352518348087:web:e98f66445780293db24eb2",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC-peKfaq8qEMeucCnMS9RAkShLuZ569Qw",
+//   authDomain: "travel-guru-b8.firebaseapp.com",
+//   projectId: "travel-guru-b8",
+//   storageBucket: "travel-guru-b8.appspot.com",
+//   messagingSenderId: "352518348087",
+//   appId: "1:352518348087:web:e98f66445780293db24eb2",
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      <h1 className='text-6xl text-white'>this is Home</h1>
+    <div className='flex'>
+      <div>
+        <h1 className='text-6xl font-bold text-white'>COX'S BAZAR</h1>
+        <p>
+          Cox's Bazar is a city, fishing port, tourism centre and district
+          headquarters in southeastern Bangladesh. It is famous mostly for its
+          long natural sandy beach, and it ...
+        </p>
+        <button className='btn btn-warning px-12 font-semibold text-white'></button>
+      </div>
+      {/* carousel  */}
+      <div>
+        <div className='carousel w-full'>
+          <div id='slide1' className='carousel-item relative w-full'>
+            <img
+              src='https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg'
+              className=' h-48'
+            />
+            {/* button  */}
+            <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
+              <a href='#slide4' className='btn btn-circle'>
+                ❮
+              </a>
+              <a href='#slide2' className='btn btn-circle'>
+                ❯
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
